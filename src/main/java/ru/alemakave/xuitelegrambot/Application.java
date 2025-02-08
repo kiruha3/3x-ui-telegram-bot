90c774da8e950ea877bd03bce28b2ca53e6cacce
Alemakave <savercat4@gmail.com>
2025-02-09 02:21:23 +1000

    * Добавлена генерация файла с информацией о билде * Исправлена ошибка, отсутствия конфигурации порта

--- src/main/java/ru/alemakave/xuitelegrambot/Application.java
+++ src/main/java/ru/alemakave/xuitelegrambot/Application.java
@@ -1,5 +1,6 @@
 package ru.alemakave.xuitelegrambot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +8,15 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Slf4j
 public class Application {
 	public static void main(String[] args) {
+		if (System.getenv("threex.connection.proxy.port").isEmpty()) {
+			System.setProperty("threex.connection.proxy.port", "-1");
+		}
+
+		log.info("Running 3x-ui Telegram Bot v" + BuildInfo.BUILD_VERSION);
+
 		SpringApplication application = new SpringApplication(Application.class);
 		application.setWebApplicationType(WebApplicationType.NONE);
 		application.run(args);
